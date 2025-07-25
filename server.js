255e9d320e8b6beb36fbc2bd2f92859e52933989
Parthiv Kumar Gunjari <parthivkumar07@gmail.com>
2025-07-25 02:28:38 -0500

    Updated production port

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => console.error('❌ MongoDB error:', err));
 
 // ✅ Server start
-const PORT = process.env.PORT || 5051;
+const PORT = process.env.NODE_ENV === 'production' ? process.env.PORT : 5051;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT} in ${process.env.NODE_ENV || 'development'} mode`);
 });
